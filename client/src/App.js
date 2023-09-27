a7972896ba8b7a212f02a7f50eb20bc6f789b4a7
Christopher Predovich <cwpredovich@yahoo.com>
2023-09-27 16:51:56 -0600

    separate main page into individual views

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
 import Students from "./components/Students";
 import Courses from "./components/Courses";
 import Grades from "./components/Grades";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [view, setView] = useState('students')
+  
+  const renderSwitch = (view) => {
+    switch(view) {
+      case 'courses':
+        return <Courses />
+      case 'search grades':
+        return <Grades />
+      default:
+        return <Students />
+    }
+  }
+  
+
   return (
-    <div>
-      <h1>Galactic Grading System</h1>
-      <p><i>Do or do not. There is no try. - Master Yoda</i></p>
-      {/* Maybe make this entire page a table with 3 columns:  Students, Courses, and Grades */}
+    <div className="app">
+      <header>
+        <h1>Galactic Grading System</h1>
+        <p><i>Do or do not. There is no try. - Master Yoda</i></p>
+      </header>
+      <nav>
+        <button className="nav-btn" onClick={() => setView('students')}>Students</button>
+        <button className="nav-btn" onClick={() => setView('courses')}>Courses</button>
+        <button className="nav-btn" onClick={() => setView('search grades')}>Search Grades</button>
+        {isLoggedIn ? <button onClick={() => setIsLoggedIn(!isLoggedIn)}>Log Out</button> : <button onClick={() => setIsLoggedIn(!isLoggedIn)}>Log In</button>}
+      </nav>
       <div className="container">
         <div className="grid">
-          <div><Students /></div>
+          {renderSwitch(view)}
+          {/* <div><Students /></div>
           <div><Courses /></div>
-          <div><Grades /></div>
+          <div><Grades /></div> */}
         </div>
       </div>
     </div>
